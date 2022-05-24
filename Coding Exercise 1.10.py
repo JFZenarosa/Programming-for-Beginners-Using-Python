50c399231f9223f30f49f77ae4201aabf9191f09
JFZenarosa <jaireh62@gmail.com>
2022-05-24 14:20:27 +0800

    Commit for coding Exercise 1.10

---
+++ Coding Exercise 1.10.py
@@ -0,0 +1,10 @@
+#Coding Exercise 1.10
+char_job = "pirate"
+char_ride = "ship"
+char_gift = "gold"
+char_pet = "parrot"
+
+print("There once was a " + char_job + " who loved adventure.")
+print("the " + char_job + " would take her " + char_ride + " to unknown places.")
+print("She brings home a lot of " + char_gift + ".")
+print("Then she goes home to her pet " + char_pet + ", Chuckles.")

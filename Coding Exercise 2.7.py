86ea29bd26fdc12e656df13cb56244af277fa623
JFZenarosa <jaireh62@gmail.com>
2022-05-24 14:26:56 +0800

    Commit for Coding Exercise 2.7

---
+++ Coding Exercise 2.7.py
@@ -0,0 +1,17 @@
+#Coding Exercise 2.7
+def slam_book(Name, Age, Fav_color, Fav_movie, Mob_num, Motto):
+  print("Name: " + Name)
+  print("Age: " + Age)
+  print("Favorite color: " + Fav_color)
+  print("Favorite movie: " + Fav_movie)
+  print("Mobile number: " + Mob_num)
+  print("My Motto in life: " + Motto)
+
+Name = input("Enter your name: ")
+Age = input("Enter you Age: ")
+Fav_color = input("Enter your favorite color: ")
+Fav_movie = input("Enter your favorite movie: ")
+Mob_num = input("Enter your mobile number: ")
+Motto = input("Enter your motto in life: ")
+
+slam_book(Name, Age, Fav_color, Fav_movie, Mob_num, Motto)
